edbf98c33d9ed5f70bb20f66457b1c126c00baa7
Rafael Viegas <rafael.viegas@riseupsolutions.com.br>
2016-08-01 13:03:14 -0300

    Testando o que realmente é necessário.

--- TDD-Csharp/TDD-Csharp.Tests/ValuerTest.cs
+++ TDD-Csharp/TDD-Csharp.Tests/ValuerTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+
 using TDD_Csharp.Chap1;
 
 namespace TDD_Csharp.Tests
@@ -7,7 +8,7 @@ namespace TDD_Csharp.Tests
      public class ValuerTest
     {
         [Test]
-        public void BidsInAscendingOrder()
+        public void MustUnderstandAuctionWithBidsInAscendingOrder()
         {
             //1a parte: cenário
             User joao = new User("Joao");
@@ -32,6 +33,44 @@ namespace TDD_Csharp.Tests
             Assert.AreEqual(highestBid, valuer.HighestBid, 0.00001);
             Assert.AreEqual(lowestBid, valuer.LowestBid, 0.00001);
 
+        }
+
+        [Test]
+        public void MustUnderstandAuctionWithJustOneBid()
+        {
+            User joao = new User("João");
+            Auction auction = new Auction("Xbox One");
+
+            auction.Propose(new Bid(joao, 1000.0));
+
+            Valuer valuer = new Valuer();
+            valuer.Valuation(auction);
+
+            Assert.AreEqual(1000, valuer.HighestBid, 0.00001);
+            Assert.AreEqual(1000, valuer.LowestBid, 0.00001);
+        }
+
+        [Test]
+        public void MustFindThreeHigher()
+        {
+            User joao = new User("João");
+            User maria = new User("Maria");
+            Auction auction = new Auction("Xbox One");
+
+            auction.Propose(new Bid(joao,100.0));
+            auction.Propose(new Bid(maria, 200.0));
+            auction.Propose(new Bid(joao, 300.0));
+            auction.Propose(new Bid(maria, 400.0));
+
+            Valuer valuer = new Valuer();
+            valuer.Valuation(auction);
+
+            var higher = valuer.ThreeHighers;
+
+            Assert.AreEqual(3,higher.Count);
+            Assert.AreEqual(400,higher[0].Value, 0.0001);
+            Assert.AreEqual(300, higher[1].Value, 0.0001);
+            Assert.AreEqual(200, higher[2].Value, 0.0001);
         }
     }
 }
--- TDD-Csharp/TDD-Csharp/Chap1/Valuer.cs
+++ TDD-Csharp/TDD-Csharp/Chap1/Valuer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TDD_Csharp.Chap1
 {
@@ -6,7 +8,7 @@ namespace TDD_Csharp.Chap1
     {
         private double highestofAll = Double.MinValue;
         private double lowestOfAll = Double.MaxValue;
-
+        private List<Bid> higher;
         public void Valuation(Auction auction)
         {
             foreach (var bid in auction.Bids)
@@ -20,7 +22,15 @@ namespace TDD_Csharp.Chap1
                     lowestOfAll = bid.Value;
                 }
             }
+            getHigher(auction);
         }
+
+        private void getHigher(Auction auction)
+        {
+            this.higher = new List<Bid>(auction.Bids.OrderByDescending(x => x.Value));
+            this.higher = higher.GetRange(0, higher.Count >= 3 ? 3 : higher.Count);
+        }
+        public List<Bid> ThreeHighers { get { return this.higher; } }
         public double HighestBid { get { return highestofAll; } }
         public double LowestBid { get { return lowestOfAll; } }
     }
